09ac18d136318f8db30294851271616c114c7458
nkola1125 <maccybeer@gmail.com>
2025-12-02 11:40:07 +0300

    Update bot and owner details in settings

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====💫 sᴛᴀʀ xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
+  BOT_NAME: 'nkola Ai', // ur bot name
+  OWNER_NAME: 'maccybeer', //your name
+  OWNER_NUMBER: '255775031403', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
